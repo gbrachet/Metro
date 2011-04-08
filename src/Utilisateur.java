6343bc12a42bb07d426f36d7f61cb48925086d12
PERRIN Maxence <maxfyr@hotmail.com>
2011-04-08 11:41:55 +0200

    getter et setters

---
+++ src/Utilisateur.java
@@ -0,0 +1,27 @@
+
+public class Utilisateur {
+
+	private int posx;
+	private int posy;
+	
+	public Utilisateur (int x, int y){
+		posx = x;
+		posy = y;
+	}
+
+	public int getPosx() {
+		return posx;
+	}
+
+	public void setPosx(int posx) {
+		this.posx = posx;
+	}
+
+	public int getPosy() {
+		return posy;
+	}
+
+	public void setPosy(int posy) {
+		this.posy = posy;
+	}
+}
